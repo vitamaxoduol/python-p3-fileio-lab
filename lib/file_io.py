47d7d2ab5e0b1775b9e2e23da5374cc56c13d8c9
Vitamax <vitamaxoduo@gmail.com>
2023-08-29 14:13:58 +0300

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as f: # Open the file in write mode
+        f.write(file_content) # Write the content to the file
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)  # Write the new content at the end of the file
+        
+    
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", "r") as f:
+        content = f.read()
+    return content
+
+
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+read_file(file_name="logfile")
